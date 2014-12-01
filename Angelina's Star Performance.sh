ccd1d7d73521b8bc75b563837e748c024e3b73f7
dvds <github-dvds@nym.hush.com>
2014-12-01 22:01:24 +0000

    Added Angelina's Star Performance script

---
+++ Angelina's Star Performance.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Copyright 2014 dvds
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# define variables
+DVD_VOLUME_LABEL="Angelinas Star Perform"
+DVD_RELEASE_DATE="11 May 2009"
+
+# create temporary file for DVD bash functions
+DVD_FUNCTIONS=$(mktemp)
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# get DVD bash functions
+wget --quiet "https://raw.githubusercontent.com/dvds/scripts/master/dvd-functions" --output-document="$DVD_FUNCTIONS"
+if [ $? -ne 0 ]
+then
+    rm --force "$DVD_FUNCTIONS"
+    exit 1
+fi
+
+# import DVD bash functions
+source "$DVD_FUNCTIONS"
+if [ $? -ne 0 ]
+then
+    rm --force "$DVD_FUNCTIONS"
+    exit 1
+fi
+
+# remove temporary file for DVD bash functions
+rm --force "$DVD_FUNCTIONS"
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# get the name of the DVD from this script
+DVD_NAME=$(get_script_name)
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# validate that the correct DVD is in the DVD drive
+check_dvd_volume_label "$DVD_VOLUME_LABEL"
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# clone the dvd
+DVD_ISO=$(clone_dvd "DVD isos" "$DVD_NAME" "$DVD_RELEASE_DATE")
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# write out the attributes of the cloned dvd
+get_file_info "$DVD_ISO"
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# rip the 1st feature title
+DVD_TITLE=$(rip_dvd_title "DVDs/$DVD_NAME" 2 1 "en" "Act I" "$DVD_RELEASE_DATE" "$DVD_ISO" "1. Act I" "$DVD_NAME")
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# write out the attributes of the ripped title
+get_matroska_file_info "$DVD_TITLE"
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# rip the 2nd feature title
+DVD_TITLE=$(rip_dvd_title "DVDs/$DVD_NAME" 3 1 "en" "Act II" "$DVD_RELEASE_DATE" "$DVD_ISO" "2. Act II" "$DVD_NAME")
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# write out the attributes of the ripped title
+get_matroska_file_info "$DVD_TITLE"
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# all ok
+exit 0
